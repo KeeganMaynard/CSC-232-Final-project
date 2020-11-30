46ad5ea1534f8e630cbb241b24fc24865dc17d5d
aarongerbrandt <74976900+aarongerbrandt@users.noreply.github.com>
2020-11-29 23:39:49 -0600

    Initial Parent Class

---
+++ Account.h
@@ -0,0 +1,17 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Account{
+    protected:
+        string username, password;
+        string firstName, lastName;
+        string phoneNumber, address;
+        string openingDate, closingDate, maturityDate;
+        double balance, interestRate;
+        int accountNumber;
+};
+
+#endif
