d9d4e4e1de28c0e62abc8b900960cf87e0c42f30
Keegan <kgm1328@live.missouristate.edu>
2020-12-01 14:35:01 -0600

    started building the cd account class

--- CdAccount.h
+++ CdAccount.h
@@ -1,1 +1,28 @@
 //header file for the cd accounts
+#ifndef CDACCOUNT_H
+#define CDACCOUNT_H
+#include <string>
+#include <iostream>
+#include "Account.h"
+using namespace std;
+
+class CdAccount : Account
+{
+private:
+	//placeholder
+public:
+	CdAccount(){}	//default constructor
+	CdAccount(int accountNumber, double balance, double interestRate)	//need to include the length of the term somehow
+};
+#endif
+
+/* CD ACCOUNT RULES
+	fixed term and fixed interest
+	early cancellation will result in a penalty fee based on the number of days remaining, no interest will be paid
+	Must have:
+		account holder's name, phone number, and address
+		interest rate
+		total $ available
+		records of all transactions with dates
+		a unique account number
+*/
