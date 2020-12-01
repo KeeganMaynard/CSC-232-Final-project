a77ed3f70701d8e89bbd1b3b3f22358e63350845
Keegan <kgm1328@live.missouristate.edu>
2020-12-01 14:23:32 -0600

    started building the checking account class

--- CheckingAccount.h
+++ CheckingAccount.h
@@ -1,1 +1,27 @@
 //Header file for the checking accounts
+#ifndef CHECKINGACCOUNT_H
+#define CHECKINGACCOUNT_H
+#include <string>
+#include <iostream>
+#include "Account.h"
+using namespace std;
+
+class CheckingAccount : public Account
+{
+private:
+	//placeholder for what needs to go here later
+public:
+	CheckingAccount () {}	//default constructor
+	CheckingAccount(int accountNumber, double balance, double interestRate);	//what is passed in from the account.h file
+};
+#endif
+
+/* CHECKING ACCOUNT RULES
+	No monthly fees and no interest
+	Must have:
+		account holder's name, phone number, and address
+		interest rate
+		total $ available
+		records of all transactions with dates
+		a unique account number 
+*/
