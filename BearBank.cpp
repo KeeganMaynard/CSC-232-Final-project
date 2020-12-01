669a8a3c270ce6bd8c6493a44d12dfa4685729b5
Keegan <kgm1328@live.missouristate.edu>
2020-12-01 14:03:09 -0600

    inserted a basic switch case inside a while loop

--- BearBank.cpp
+++ BearBank.cpp
@@ -1,10 +1,13 @@
 //branch version of bearbank
 #include <iostream>
+#include <string>
 #include "BankOfficials.h"
 using namespace std;
 
 int main()
 {
+	bool run = true;
+	/*
 	//cout << "Initial test statement" << endl;
 	BankOfficials <string> employeeList;
 	employeeList.hireEmployee("Keegan");
@@ -14,4 +17,60 @@ int main()
 	//employeeList.fireEmployee("Keegan");  //delete from root works
 	employeeList.fireEmployee("Nathan");
 	//employeeList.loginIn("Nathan");		//will break the program, need invalid input handler in function
+	*/
+
+	while (run)
+	{
+		cout << "Enter a number: ";
+		string input = "";
+		int option = -1;
+		getline(cin, input);
+
+		try   //validate input
+		{
+			if (input.length() == 1)
+			{
+				option = stoi(input);
+			}
+			else
+			{
+				throw 1;
+			}
+		}
+		catch (...)
+		{
+			//display statement will be different after full implementation
+			cout << "Invalid input entered." << endl;
+			continue;
+		}
+
+		switch (option)
+		{
+		case 1:
+		{
+			cout << "1 was entered as the input" << endl;
+			break;
+		}
+
+		case 2:
+		{
+			cout << "2 was entered as the input" << endl;
+			break;
+		}
+		
+		case 3:
+		{
+			cout << "3 was entered as the input" << endl;
+			run = false;
+			break;
+		}
+
+		default:
+		{
+			cout << "Invalid input" << endl;
+			run = false;
+			break;
+		}
+		}
+	}
 }
