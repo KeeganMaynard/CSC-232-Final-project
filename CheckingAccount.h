346daa662a571b4c18ea508660abd6ddad7fd0a7
Keegan <kgm1328@live.missouristate.edu>
2020-12-01 20:02:19 -0600

    included basic withdraw and deposit functions

--- CheckingAccount.h
+++ CheckingAccount.h
@@ -13,7 +13,80 @@ private:
 public:
 	CheckingAccount () {}	//default constructor
 	CheckingAccount(int accountNumber, double balance, double interestRate);	//what is passed in from the account.h file
+	string deposit(double amount);
+	string withdraw(double amount);
+	void closeAccount();
 };
+
+string CheckingAccount::deposit(double amount)
+{
+	if (isOpen())
+	{
+		try
+		{
+			if (amount < .01)
+			{
+				throw "Invalid operation. You cannot deposite a negative or zero amount" << endl;
+			}
+			else
+			{
+				//setbalance(getbalance() + amount);
+			}
+
+			string statement = "$" << amount << " has been depositited into your checking account" << endl;
+			return statement;
+		}
+		catch (string err)
+		{
+			return err;
+		}
+	}
+	else
+	{
+		return "The account is closed at this time\n";
+	}
+}
+
+string CheckingAccount::withdraw(double amount)
+{
+	if (isOpen())
+	{
+		try
+		{
+			if (amount < 0.01)
+			{
+				throw "Invalid operation. You cannot withdraw a negative or zero amount";
+			}
+		}
+		catch (string err)
+		{
+			return err;
+		}
+
+		double newBalance = getBalance() - amount;
+
+		if (newBalance < 0.0)
+		{
+			string statement = "Account has insufficient funds to withdraw $" << amount << endl;
+			return statement;
+		}
+		else
+		{
+			setBalance(newBalance);
+		}
+		string statement = "$" << amount << " has been withdrawn from your account" << endl;
+		return statement;
+	}
+	else
+	{
+		return "The account is closed at this time\n";
+	}
+}
+
+void CheckingAccount::closeAccount()
+{
+	setIsClosed(true);
+}
 #endif
 
 /* CHECKING ACCOUNT RULES
