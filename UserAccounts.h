0dc522c177a0028d8402d1f41ea738eacd5b3497
Keegan <kgm1328@live.missouristate.edu>
2020-12-02 19:22:47 -0600

    built the class based on Users.h parent class

--- UserAccounts.h
+++ UserAccounts.h
@@ -1,3 +1,25 @@
-//Header file for the user accounts
+#ifndef USERACCOUNTS_H
+#define USERACCOUNTS_H
+#include "Users.h"
 
-//this file will be used to store users information and functions
+class UserAccounts : public Users
+{
+private:
+	string memberAddress;
+	string memberPhone;
+public:
+	UserAccounts();
+	UserAccounts(string name, string ID, string password, string type, string login, string address, string phone);
+	UserAccounts(string name, string ID, string password, string type, string address, string phone);
+
+	string getAddress();
+	string getPhone();
+	void setAddress(string address);
+	void setPhone(string phone);
+
+	void saveUser();
+	void createUser(string fileID);
+	void history(vector<string> accounts);
+	void display();
+};
+#endif
