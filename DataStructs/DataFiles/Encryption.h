6bb67c2187656f8fb5df06bf60b5370bda55f25e
aarongerbrandt <74976900+aarongerbrandt@users.noreply.github.com>
2020-12-04 22:29:47 -0600

    Added encrypt and decrypt functions

--- DataStructs/DataFiles/Encryption.h
+++ DataStructs/DataFiles/Encryption.h
@@ -2,7 +2,21 @@
 #define ENCRYPTION_H
 #include <iostream>
 #include <string>
-#include <vector>
-#include <sstream>
 using namespace std;
+
+string encrypt(string s){
+    for (int i = 0; i < s.length(); i++)
+    {
+        s[i] = s[i]+45; //Adding 45 to ASCII code
+    }
+    cout << s << endl;
+}
+
+string decrypt(string s){
+    for (int i = 0; i < s.length(); i++)
+    {
+        s[i] = s[i]-45; //Changing ASCII code back to normal
+    }
+}
+
 #endif
